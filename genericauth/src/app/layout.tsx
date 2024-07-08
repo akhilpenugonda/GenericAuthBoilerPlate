e7eab37a85522484301baf966e0deb01ea23539c
Akhil Kumar PeerIslands <akhilkumar.penugonda@peerislands.io>
2024-07-07 22:01:12 -0500

    Added Shadcn UI properly

--- genericauth/src/app/layout.tsx
+++ genericauth/src/app/layout.tsx
@@ -1,15 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Inter as FontSans } from "next/font/google";
-import { cn } from "@/lib/utils";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
-const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,10 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
