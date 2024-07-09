a7617d9b6fb67311cedc647a53c3360f1d895013
akhilpenugonda <akhilnani14@gmail.com>
2024-07-09 16:45:55 -0500

    Added read me and common message in login page

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # GenericAuthBoilerPlate
 Generic Next Auth Boiler Plate with Shad CN
+
+
+npx prisma generate
+npx prisma db push
--- genericauth/src/app/(auth)/(signin)/page.tsx
+++ genericauth/src/app/(auth)/(signin)/page.tsx
@@ -41,9 +41,7 @@ export default function AuthenticationPage() {
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
             <p className="text-lg">
-              &ldquo;This library has saved me countless hours of work and
-              helped me deliver stunning designs to my clients faster than ever
-              before.&rdquo;
+              &ldquo;A simple library with auth and beautiful dash board ready to be used&rdquo;
             </p>
             <footer className="text-sm">Sofia Davis</footer>
           </blockquote>
